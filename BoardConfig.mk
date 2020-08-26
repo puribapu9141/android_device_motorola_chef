10cc95c56310e23229d450dc69e27e7a0f8b39b4
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-08-26 09:51:40 +0200

    chef: Use 4.14 defconfig
    Change-Id: I6b78a1a5741c8fa0de469ef4cee6956dc60b278c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_chef_defconfig
+TARGET_KERNEL_CONFIG := chef_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
